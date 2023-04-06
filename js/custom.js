e2ffdf8e2e46c05cb08e49c3629cf513126faec3
unknown <you@example.com[200~you@example.com>
2023-04-06 16:53:21 +0500

    Eprojext

---
+++ js/custom.js
@@ -0,0 +1,122 @@
+$(document).ready(function () {
+    $(".counter").counterUp({
+        delay: 10,
+        time: 1000
+
+    })
+});
+
+$(document).ready(function () {
+    $(".all").click(function () {
+        $(".historic-image").show();
+        $(".awarded-image").show();
+        $(".iconic-image").show();
+        $(".modern-image").show();
+        $(".longest-image").show();
+        $(".highest-image").show();
+        $(".oldest-image").show();
+        $(".ad").show();
+        $(".Gallery1").show();
+        $(".about-us").show();
+        $(".count").show();
+        $(".deneb_cta").show();
+
+
+    });
+    $(".historic").click(function () {
+        $(".historic-image").show();
+        $(".awarded-image").hide();
+        $(".iconic-image").hide();
+        $(".modern-image").hide();
+        $(".longest-image").hide();
+        $(".highest-image").hide();
+        $(".oldest-image").hide();
+        $(".Gallery1").hide();
+        $(".about-us").hide();
+        $(".count").hide();
+        $(".deneb_cta").hide();
+
+    });
+    $(".awarded").click(function () {
+        $(".historic-image").hide();
+        $(".awarded-image").show();
+        $(".iconic-image").hide();
+        $(".modern-image").hide();
+        $(".longest-image").hide();
+        $(".highest-image").hide();
+        $(".oldest-image").hide();
+        $(".Gallery1").hide();
+        $(".about-us").hide();
+        $(".count").hide();
+        $(".deneb_cta").hide();
+    });
+    $(".iconic").click(function () {
+        $(".historic-image").show();
+        $(".awarded-image").hide();
+        $(".iconic-image").show();
+        $(".modern-image").hide();
+        $(".longest-image").hide();
+        $(".highest-image").hide();
+        $(".oldest-image").hide();
+        $(".ad").hide();
+        $(".Gallery1").hide();
+        $(".about-us").hide();
+        $(".count").hide();
+        $(".deneb_cta").hide();
+    });
+    $(".modern").click(function () {
+        $(".historic-image").hide();
+        $(".awarded-image").hide();
+        $(".iconic-image").hide();
+        $(".modern-image").show();
+        $(".longest-image").hide();
+        $(".highest-image").hide();
+        $(".oldest-image").hide();
+        $(".Gallery1").hide();
+        $(".about-us").hide();
+        $(".count").hide();
+        $(".deneb_cta").hide();
+    });
+    $(".longest").click(function () {
+        $(".historic-image").hide();
+        $(".awarded-image").hide();
+        $(".iconic-image").hide();
+        $(".modern-image").hide();
+        $(".longest-image").show();
+        $(".highest-image").hide();
+        $(".oldest-image").hide();
+        $(".Gallery1").hide();
+        $(".about-us").hide();
+        $(".count").hide();
+        $(".deneb_cta").hide();
+    });
+    $(".highest").click(function () {
+        $(".historic-image").hide();
+        $(".awarded-image").hide();
+        $(".iconic-image").hide();
+        $(".modern-image").hide();
+        $(".longest-image").hide();
+        $(".highest-image").show();
+        $(".oldest-image").hide();
+        $(".Gallery1").hide();
+        $(".about-us").hide();
+        $(".count").hide();
+        $(".deneb_cta").hide();
+    });
+    $(".oldest").click(function () {
+        $(".historic-image").hide();
+        $(".awarded-image").hide();
+        $(".iconic-image").hide();
+        $(".modern-image").hide();
+        $(".longest-image").hide();
+        $(".highest-image").hide();
+        $(".oldest-image").show();
+        $(".Gallery1").hide();
+        $(".about-us").hide();
+        $(".count").hide();
+        $(".deneb_cta").hide();
+    });
+
+
+});
+
